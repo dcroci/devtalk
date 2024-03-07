92ac69e7885518b42324f47b45c4092861e22223
Dominik Croci <dominikrcroci@gmail.com>
2024-03-07 16:12:17 -0700

    add nextui, framer motion, prettier-tailwind (#10)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,33 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        almostBlack: '#09090B',
-        almostWhite: '#FAFAFA',
-        medGray: '#A1A1AA',
-        darkGray: '#27272A',
-        purple: '#8900F2',
+        almostBlack: "#09090B",
+        almostWhite: "#FAFAFA",
+        medGray: "#A1A1AA",
+        darkGray: "#27272A",
+        purple: "#8900F2",
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
