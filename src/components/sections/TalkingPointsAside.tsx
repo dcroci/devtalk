f31a99324616e29feaf1207a4eefba0f4a025e28
Dominik Croci <dominikrcroci@gmail.com>
2024-03-07 16:14:27 -0700

    create talking points component (#13)

---
+++ src/components/sections/TalkingPointsAside.tsx
@@ -0,0 +1,500 @@
+function TalkingPointsAside() {
+  return (
+    <aside className="h-fit w-full rounded bg-darkGray">
+      <div className="flex items-center">
+        <img
+          className="w-[40px]"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png"
+          alt=""
+        />
+        <h2 className="ml-16 text-[16px] font-semibold text-almostWhite">
+          Talking Points
+        </h2>
+      </div>
+      <div className="grid grid-cols-1 gap-4 p-4">
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="mx-auto flex   items-center gap-4 rounded bg-almostBlack p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              className="h-[40px] w-[40px] stroke-purple"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <div>
+              <h3
+                className=" text-[16px] text-almostWhite
+          "
+              >
+                How do I filter an array?
+              </h3>
+              <div>
+                <p className="text-[14px] text-medGray">
+                  16 Comments · 12 Likes
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
+}
+
+export default TalkingPointsAside;
