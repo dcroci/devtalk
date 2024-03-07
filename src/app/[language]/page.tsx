d3b931d88883e39ed1eddd0022450ec600b03e1f
Dominik Croci <dominikrcroci@gmail.com>
2024-03-07 16:13:29 -0700

    implement components into /language grid (#14)

---
+++ src/app/[language]/page.tsx
@@ -0,0 +1,14 @@
+import LanguageMain from "@/components/sections/LanguageMain";
+import SideNav from "@/components/sections/SideNav";
+import TalkingPointsAside from "@/components/sections/TalkingPointsAside";
+function ShowLanguagePage() {
+  return (
+    <>
+      <SideNav />
+      <LanguageMain />
+      <TalkingPointsAside />
+    </>
+  );
+}
+
+export default ShowLanguagePage;
