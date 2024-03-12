2f574d17b0cfe36dacce5972f245767056500710
Dominik Croci <dominikrcroci@gmail.com>
2024-03-11 23:12:23 -0700

    create sidenav with links (#23)

--- src/components/sections/SideNav.tsx
+++ src/components/sections/SideNav.tsx
@@ -2,7 +2,9 @@
 import { Accordion, AccordionItem } from "@nextui-org/react";
 import Link from "next/link";
 
-export default function SideNav() {
+import { Language } from "./LanguageMain";
+
+export default function SideNav({ language }: Language) {
   const itemClasses = {
     base: "py-0 w-full col-start-1 col-end-2",
     title: "font-normal text-[14px] text-almostWhite",
@@ -17,29 +19,53 @@ export default function SideNav() {
       itemClasses={itemClasses}
       selectionMode="multiple"
     >
-      <AccordionItem key="1" aria-label="On This Page" title="On This Page">
+      <AccordionItem
+        key="1"
+        aria-label={`${language.name} Home`}
+        title={`${language.name} Home`}
+      >
         <ul className="ml-4 flex flex-col gap-2 font-normal text-medGray">
-          <li>
-            <Link href="#useCase">Use Case</Link>
+          <li className="hover:underline">
+            <Link href={`/${language.name}/#useCase`}>Use Case</Link>
           </li>
-          <Link href="#history">History</Link>
+          <Link href={`/${language.name}/#history`}>History</Link>
           <li>
-            <Link href="#documentation">Documentation</Link>
+            <Link href={`/${language.name}/#documentation`}>Documentation</Link>
           </li>
           <li>
             {" "}
-            <Link href="#latestRelease">Latest Release</Link>
+            <Link href={`/${language.name}/#latestRelease`}>
+              Latest Release
+            </Link>
           </li>
         </ul>
       </AccordionItem>
       <AccordionItem key="2" aria-label="Resources" title="Resources">
         <ul className="ml-4 flex flex-col gap-2 font-normal text-medGray">
-          <li>Content Creators</li>
-          <li>Courses</li>
-          <li>Books</li>
-          <li>Videos</li>
-          <li>Interview Prep</li>
-          <li>Job Board</li>
+          <li>
+            <Link href="#contentCreators">Content Creators</Link>
+          </li>
+          <li>
+            <Link href="#courses">Courses</Link>
+          </li>
+          <li>
+            <Link href="#books">Books</Link>
+          </li>
+          <li>
+            <Link href="#videos">Videos</Link>
+          </li>
+          <li className="flex w-fit items-center gap-2 ">
+            Interview Prep
+            <p className=" rounded border-2 border-purple bg-purple/25 px-1 text-[10px]">
+              Soon
+            </p>
+          </li>
+          <li className="flex w-fit items-center gap-2 ">
+            Job Board
+            <p className=" rounded border-2 border-purple bg-purple/25 px-1 text-[10px]">
+              Soon
+            </p>
+          </li>
         </ul>
       </AccordionItem>
       <AccordionItem key="4" aria-label="Talking Points" title="Talking Points">
@@ -51,8 +77,16 @@ export default function SideNav() {
       </AccordionItem>
       <AccordionItem key="3" aria-label="Accordion 3" title="Snippets">
         <ul className="ml-4 flex flex-col gap-2 font-normal text-medGray">
-          <li>New</li>
-          <li>Popular</li>
+          <li>
+            <Link href={`/${language.name.toLowerCase()}/snippets/new`}>
+              New
+            </Link>
+          </li>
+          <li>
+            <Link href={`/${language.name.toLowerCase()}/snippets/`}>
+              Popular
+            </Link>
+          </li>
           <li>Create</li>
         </ul>
       </AccordionItem>
