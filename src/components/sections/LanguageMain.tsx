0821a9f68eda21bd46e3a9c17e5dc74dbd458e82
Dominik Croci <dominikrcroci@gmail.com>
2024-03-19 11:35:48 -0700

    implement info into main sections (#84)

--- src/components/sections/LanguageMain.tsx
+++ src/components/sections/LanguageMain.tsx
@@ -47,9 +47,10 @@ function LanguageMain({ language }: any) {
           Documentation
         </h2>
         <p className="text-medGray">
-          Dive into coding with Dev Talk, where every line of code connects you
-          to a world of resources, expert insights, and a vibrant developer
-          community.
+          View popular {language.name} documentation{" "}
+          <a href={language.documentation} className="border-b-2 border-purple">
+            here
+          </a>
         </p>
       </section>
       <section id="latestRelease" className="border-b-1 border-darkGray pb-4">
@@ -57,9 +58,8 @@ function LanguageMain({ language }: any) {
           Latest Release
         </h2>
         <p className="text-medGray">
-          Dive into coding with Dev Talk, where every line of code connects you
-          to a world of resources, expert insights, and a vibrant developer
-          community.
+          {language.name} is currently operating at version{" "}
+          {language.latestRelease}
         </p>
       </section>
       <section id="resources" className="flex flex-col gap-4">
@@ -84,13 +84,13 @@ function LanguageMain({ language }: any) {
             </p>
           </section>
           {language.creator1Name && (
-            <section className="grid grid-cols-4">
+            <section className="grid grid-cols-1 gap-2 md:grid-cols-4">
               <img
                 src={language.creator1Image}
                 alt=""
-                className="col-start-1 col-end-1 rounded-full"
+                className=" mx-auto rounded-full md:col-start-1 md:col-end-1"
               />
-              <div className="col-start-2 col-end-5">
+              <div className="md:col-start-2 md:col-end-5">
                 <h3 className="text-[18px] font-semibold text-almostWhite">
                   {language.creator1Name}
                 </h3>
@@ -110,13 +110,13 @@ function LanguageMain({ language }: any) {
             </section>
           )}
           {language.creator2Name && (
-            <section className="grid grid-cols-4">
+            <section className="grid grid-cols-1 gap-2 md:grid-cols-4">
               <img
                 src={language.creator2Image}
                 alt=""
-                className="col-start-1 col-end-1 rounded-full"
+                className=" mx-auto rounded-full md:col-start-1 md:col-end-1"
               />
-              <div className="col-start-2 col-end-5">
+              <div className="md:col-start-2 md:col-end-5">
                 <h3 className="text-[18px] font-semibold text-almostWhite">
                   {language.creator2Name}
                 </h3>
@@ -136,13 +136,13 @@ function LanguageMain({ language }: any) {
             </section>
           )}
           {language.creator3Name && (
-            <section className="grid grid-cols-4">
+            <section className="grid grid-cols-1 gap-2 md:grid-cols-4">
               <img
                 src={language.creator3Image}
                 alt=""
-                className="col-start-1 col-end-1 rounded-full"
+                className=" mx-auto rounded-full md:col-start-1 md:col-end-1"
               />
-              <div className="col-start-2 col-end-5">
+              <div className="md:col-start-2 md:col-end-5">
                 <h3 className="text-[18px] font-semibold text-almostWhite">
                   {language.creator3Name}
                 </h3>
@@ -162,6 +162,7 @@ function LanguageMain({ language }: any) {
             </section>
           )}
         </section>
+
         <section
           id="courses"
           className="grid grid-cols-2 gap-4 border-b-1 border-darkGray pb-4"
@@ -176,53 +177,101 @@ function LanguageMain({ language }: any) {
               developer community.
             </p>
           </section>
-
-          <section className="grid grid-cols-1 gap-4">
-            <img
-              src="https://miro.medium.com/v2/resize:fit:750/1*guF3jJePGofWd8dkdzuaqA.jpeg"
-              alt=""
-            />
-            <div>
-              <h3 className="text-[18px] font-semibold text-almostWhite">
-                The Complete JavaScript Course 2024: From Zero to Expert!
-              </h3>
-              <p className="text-medGray">
-                Dive into coding with Dev Talk, where every line of code
-                connects you to a world of resources, expert insights, and a
-                vibrant developer community. Dive into coding with Dev Talk,
-                where every line of code connects you to a world of resources,
-                expert insights, and a vibrant developer community.
-              </p>
-              <Link href={"/"} className=" ml-auto flex w-fit ">
-                <Button className="rounded bg-purple px-4 py-2 font-semibold text-almostWhite">
-                  View Course
-                </Button>
-              </Link>
-            </div>
+          {language.course1Name && (
+            <section className="grid grid-cols-1 gap-4">
+              <img src={language.course1Image} alt="" />
+              <div>
+                <h3 className="text-[18px] font-semibold text-almostWhite">
+                  {language.course1Name}
+                </h3>
+                <p className="text-medGray">{language.course1Desc}</p>
+                <a
+                  href={language.course1Link}
+                  className=" ml-auto flex w-fit "
+                  target="_blank"
+                >
+                  <Button className="rounded bg-purple px-4 py-2 font-semibold text-almostWhite">
+                    View Course
+                  </Button>
+                </a>
+              </div>
+            </section>
+          )}
+          {language.course2Name && (
+            <section className="grid grid-cols-1">
+              <img src={language.course2Image} alt="" className="w-full" />
+              <div>
+                <h3 className="text-[18px] font-semibold text-almostWhite">
+                  {language.course2Name}
+                </h3>
+                <p className="text-medGray">{language.course2Desc}</p>
+                <a
+                  href={language.course2Link}
+                  className=" ml-auto flex w-fit "
+                  target="_blank"
+                >
+                  <Button className="rounded bg-purple px-4 py-2 font-semibold text-almostWhite">
+                    View Course
+                  </Button>
+                </a>
+              </div>
+            </section>
+          )}
+        </section>
+        <section
+          id="books"
+          className="grid grid-cols-2 gap-4 border-b-1 border-darkGray pb-4"
+        >
+          <section className="col-span-full">
+            <h2 className="text-[24px] font-semibold text-almostWhite">
+              Popular Books
+            </h2>
+            <p className="text-medGray">
+              Dive into coding with Dev Talk, where every line of code connects
+              you to a world of resources, expert insights, and a vibrant
+              developer community.
+            </p>
           </section>
-          <section className="grid grid-cols-1">
-            <img
-              src="https://miro.medium.com/v2/resize:fit:750/1*guF3jJePGofWd8dkdzuaqA.jpeg"
-              alt=""
-            />
-            <div>
-              <h3 className="text-[18px] font-semibold text-almostWhite">
-                The Complete JavaScript Course 2024: From Zero to Expert!
-              </h3>
-              <p className="text-medGray">
-                Dive into coding with Dev Talk, where every line of code
-                connects you to a world of resources, expert insights, and a
-                vibrant developer community. Dive into coding with Dev Talk,
-                where every line of code connects you to a world of resources,
-                expert insights, and a vibrant developer community.
-              </p>
-              <Link href={"/"} className=" ml-auto flex w-fit ">
-                <Button className="rounded bg-purple px-4 py-2 font-semibold text-almostWhite">
-                  View Course
-                </Button>
-              </Link>
-            </div>
-          </section>
+          {language.book1Name && (
+            <section className="flex flex-col">
+              <img src={language.book1Image} alt="" />
+              <div>
+                <h3 className="text-[18px] font-semibold text-almostWhite">
+                  {language.book1Name}
+                </h3>
+                <p className="text-medGray">{language.book1Desc}</p>
+                <a
+                  href={language.book1Link}
+                  className=" ml-auto flex w-fit "
+                  target="_blank"
+                >
+                  <Button className="rounded bg-purple px-4 py-2 font-semibold text-almostWhite">
+                    View Book
+                  </Button>
+                </a>
+              </div>
+            </section>
+          )}
+          {language.book2Name && (
+            <section className="flex flex-col">
+              <img src={language.book2Image} alt="" className="w-full" />
+              <div>
+                <h3 className="text-[18px] font-semibold text-almostWhite">
+                  {language.book2Name}
+                </h3>
+                <p className="text-medGray">{language.book2Desc}</p>
+                <a
+                  href={language.book2Link}
+                  className=" ml-auto flex w-fit "
+                  target="_blank"
+                >
+                  <Button className="rounded bg-purple px-4 py-2 font-semibold text-almostWhite">
+                    View Book
+                  </Button>
+                </a>
+              </div>
+            </section>
+          )}
         </section>
       </section>
     </main>
