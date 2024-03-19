79c0c4548addedf50a27657ce0b308e9cbdfce32
Dominik Croci <dominikrcroci@gmail.com>
2024-03-18 21:05:37 -0700

    add skeleton to snippetcard (#73)

--- src/components/snippets/SnippetCard.tsx
+++ src/components/snippets/SnippetCard.tsx
@@ -5,37 +5,48 @@ import {
   CardBody,
   CardFooter,
   Divider,
+  Skeleton,
 } from "@nextui-org/react";
 import Link from "next/link";
+import { useState } from "react";
 interface SnippetCardProps {
   languageName: string;
   snippet: any;
   logoUrl: string;
 }
 function SnippetCard({ snippet, languageName, logoUrl }: SnippetCardProps) {
+  const [isLoaded, setIsLoaded] = useState(() => {
+    if (snippet) {
+      return true;
+    } else {
+      return false;
+    }
+  });
   return (
-    <Link
-      href={`/${languageName.toLowerCase()}/snippets/${snippet.id}`}
-      className="h-fit"
-    >
-      <Card className="max-w-[400px] bg-almostBlack">
-        <CardHeader className="flex gap-3 bg-[#18181b] text-white">
-          <img src={`${logoUrl}`} alt="" className="w-8" />
+    <Skeleton isLoaded={isLoaded} className="rounded">
+      <Link
+        href={`/${languageName.toLowerCase()}/snippets/${snippet.id}`}
+        className="h-fit"
+      >
+        <Card className="max-w-[400px] bg-almostBlack">
+          <CardHeader className="flex gap-3 bg-[#18181b] text-white">
+            <img src={`${logoUrl}`} alt="" className="w-8" />
 
-          <div className="flex flex-col">
-            <p className="font-medium">{snippet.title}</p>
-          </div>
-        </CardHeader>
-        <Divider />
-        <CardBody className="bg-[#18181b] text-white">
-          <p>This is a function that does this one thing</p>
-        </CardBody>
-        <Divider />
-        <CardFooter className="bg-[#18181b] text-white">
-          <Link href={`snippets/${snippet.id}`}>View Code Snippet</Link>
-        </CardFooter>
-      </Card>
-    </Link>
+            <div className="flex flex-col">
+              <p className="font-medium">{snippet.title}</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody className="bg-[#18181b] text-white">
+            <p>This is a function that does this one thing</p>
+          </CardBody>
+          <Divider />
+          <CardFooter className="bg-[#18181b] text-white">
+            <p>View Code Snippet</p>
+          </CardFooter>
+        </Card>
+      </Link>
+    </Skeleton>
   );
 }
 
