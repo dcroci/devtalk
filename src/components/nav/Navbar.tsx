13355c5d6057772f63fd32008411d2a73dc04bd4
Dominik Croci <dominikrcroci@gmail.com>
2024-03-19 11:35:09 -0700

    sort languages by abc (#83)

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -13,7 +13,11 @@ import Image from "next/image";
 import HamburgerMenu from "./HamburgerMenu";
 
 async function Navbar() {
-  const languages = await db.language.findMany({});
+  const languages = await db.language.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
 
   const session = await auth();
 
@@ -89,7 +93,21 @@ async function Navbar() {
                 action={actions.signOut}
                 className="text-almostWhite hover:cursor-pointer"
               >
-                <button type="submit">Sign Out</button>
+                <button type="submit" className="flex items-center gap-2">
+                  Sign Out{" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    className="h-6 w-6 stroke-almostWhite"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
+                    />
+                  </svg>
+                </button>
               </form>
             </PopoverContent>
           </Popover>
