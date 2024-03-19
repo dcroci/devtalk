d5550273971fa5458290f1e2f1a49a71e74e061a
Dominik Croci <dominikrcroci@gmail.com>
2024-03-18 20:37:13 -0700

    create actions for create & delete project (#64)

---
+++ src/actions/projects.ts
@@ -0,0 +1,104 @@
+"use server";
+import type { Project } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { auth } from "@/auth";
+import { db } from "@/app/db";
+
+//CREATE A PROJECT
+const createProjectSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+  imgUrl: z.string(),
+  //   languages: z.any(),
+});
+interface CreateProjectFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    imgUrl?: string[];
+    // languages?: any;
+    _form?: string[];
+  };
+}
+export async function createProject(
+  languageId: string,
+  formState: CreateProjectFormState,
+  formData: FormData,
+): Promise<CreateProjectFormState> {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const result = createProjectSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+    imgUrl: formData.get("imgUrl"),
+    // languages: formData.get("languages"),
+  });
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  //   console.log("FORM----------", formData.get("languages"));
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to create a project"],
+      },
+    };
+  }
+
+  const language = await db.language.findFirst({
+    where: {
+      id: languageId,
+    },
+  });
+
+  if (!language) {
+    return {
+      errors: {
+        _form: ["Could not find language"],
+      },
+    };
+  }
+
+  let project: Project;
+  try {
+    project = await db.project.create({
+      data: {
+        title: result.data.title,
+        desc: result.data.content,
+        imgUrl: result.data.imgUrl,
+        userId: session.user.id,
+        languageId: language.id,
+        likes: 0,
+        // technologies: result.data.languages,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["Failed to create talking point"],
+        },
+      };
+    }
+  }
+  revalidatePath(`/${language?.name.toLowerCase()}/showcase`);
+  redirect(`/${language?.name.toLowerCase()}/showcase/${project.id}`);
+}
+export async function deleteProject(id: string, language: string) {
+  await db.project.delete({
+    where: { id: id },
+  });
+  revalidatePath(`/${language.toLowerCase()}/showcase`);
+  redirect(`/${language.toLowerCase()}/showcase`);
+}
