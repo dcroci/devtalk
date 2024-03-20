88c2456bd9f0714391f5017cd761440524c80402
Dominik Croci <dominikrcroci@gmail.com>
2024-03-20 00:29:47 -0700

    add placeholder content to home page (#97)

--- src/components/home/IntroSection.tsx
+++ src/components/home/IntroSection.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 function IntroSection() {
   return (
     <section className="col-span-full mt-[80px] grid grid-cols-5">
@@ -10,75 +11,61 @@ function IntroSection() {
         a world of resources, expert insights, and a vibrant developer
         community.
       </p>
-      <div className="col-span-full grid grid-cols-1 gap-4 sm:grid-cols-3">
-        <section className="w-[1000px min-h-[380px] rounded border-2 border-darkGray p-[24px] ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            className="mx-auto h-60 w-40  stroke-almostWhite"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
-            />
-          </svg>
-          <h3 className="text-[20px] font-semibold text-almostWhite">
-            Create Talking Points
-          </h3>
-          <p className="text-[14px] text-medGray">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            impedit, placeat aliquam rem asperiores, necessitatibus illo alias,
-            sed ratione tempore ut. Nemo perspiciatis eaque delectus id
-            voluptates optio asperiores. Numquam.
-          </p>
+      <div className="col-span-full grid grid-cols-1 place-items-center gap-4 lg:grid-cols-2 2xl:grid-cols-3">
+        <section className="flex  h-full min-h-[380px] w-[500px]  flex-col justify-between gap-2 rounded border-2 border-darkGray p-[24px] transition-all duration-1000 hover:shadow-md hover:shadow-purple">
+          <Image
+            src="/assets/tp.png"
+            alt="image of a talking point"
+            width={400}
+            height={400}
+          />
+          <div>
+            <h3 className="text-[20px] font-semibold text-almostWhite">
+              Create Talking Points
+            </h3>
+            <p className="text-[14px] leading-relaxed text-medGray">
+              Talking Points are your space to spark discussions and share
+              insights on web development topics. Share your thoughts, ask
+              questions, and connect with peers to exchange knowledge and ideas
+              in a vibrant community.
+            </p>
+          </div>
         </section>
-        <section className="w-[1000px min-h-[380px] rounded border-2 border-darkGray p-[24px] ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            className="mx-auto h-60 w-40  stroke-almostWhite"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
-            />
-          </svg>
-          <h3 className="text-[20px] font-semibold text-almostWhite">
-            Discover Code Snippets
-          </h3>
-          <p className="text-[14px] text-medGray">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            impedit, placeat aliquam rem asperiores, necessitatibus illo alias,
-            sed ratione tempore ut. Nemo perspiciatis eaque delectus id
-            voluptates optio asperiores. Numquam.
-          </p>
+        <section className="flex  h-full min-h-[380px] w-[500px]  flex-col justify-between gap-2 rounded border-2 border-darkGray p-[24px] transition-all duration-1000 hover:shadow-md hover:shadow-purple">
+          <Image
+            src="/assets/snippet.png"
+            alt="image of a code snippet"
+            width={400}
+            height={400}
+          />
+          <div>
+            <h3 className="text-[20px] font-semibold text-almostWhite">
+              Discover Code Snippets
+            </h3>
+            <p className="text-[14px] leading-relaxed text-medGray">
+              Find and share concise, ready-to-use code pieces across various
+              languages and frameworks. Enhance your projects and learn new
+              tricks with this curated selection of code snippets.
+            </p>
+          </div>
         </section>
-        <section className="w-[1000px min-h-[380px] rounded border-2 border-darkGray p-[24px] ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            className="mx-auto h-60 w-40  stroke-almostWhite"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
-            />
-          </svg>
-          <h3 className="text-[20px] font-semibold text-almostWhite">
-            Share Your Projects
-          </h3>
-          <p className="text-[14px] text-medGray">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            impedit, placeat aliquam rem asperiores, necessitatibus illo alias,
-            sed ratione tempore ut. Nemo perspiciatis eaque delectus id
-            voluptates optio asperiores. Numquam.
-          </p>
+        <section className="flex  h-full min-h-[380px] w-[500px]  flex-col justify-between gap-2 rounded border-2 border-darkGray p-[24px] transition-all duration-1000 hover:shadow-md hover:shadow-purple">
+          <Image
+            src="/assets/project.png"
+            alt="image of a code snippet"
+            width={400}
+            height={400}
+          />
+          <div>
+            <h3 className="text-[20px] font-semibold text-almostWhite">
+              Share Your Projects
+            </h3>
+            <p className="text-[14px] leading-relaxed text-medGray">
+              Showcase your work and gain feedback from the community. Highlight
+              your achievements, the technologies you used, and the obstacles
+              you overcame. Connect, inspire, and grow with fellow developers.
+            </p>
+          </div>
         </section>
       </div>
     </section>
