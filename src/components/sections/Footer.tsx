4ae9445421310b64ac256a8c57908931dc2be8e1
Dominik Croci <dominikrcroci@gmail.com>
2024-03-21 13:31:10 -0700

    add link to portfolio in footer (#110)

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,7 +1,17 @@
 function Footer() {
   return (
     <footer className="col-span-full flex h-[70px] items-center border-t-1 border-darkGray text-medGray">
-      <p>Built by Dominik Croci.</p>
+      <p>
+        Built by{" "}
+        <a
+          href="https://dominikcroci.com"
+          target="_blank"
+          className="hover:underline"
+        >
+          Dominik Croci
+        </a>
+        .
+      </p>
     </footer>
   );
 }
