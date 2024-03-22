a0e10c87ebea14cf1dbb85b8ddae1166184120bb
Dominik Croci <dominikrcroci@gmail.com>
2024-03-21 22:28:55 -0700

    add filter logic with search params (#117)

--- src/components/common/Filter.tsx
+++ src/components/common/Filter.tsx
@@ -1,19 +1,31 @@
+"use client";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
 function Filter() {
+  const searchParams = useSearchParams();
+  const selectedFilter = searchParams.get("filter") || "new";
+
+  const filters = ["new", "likes", "comments", "oldest"];
   return (
     <div className="mb-4 mt-2 w-full rounded bg-darkGray">
       <ul className="grid w-full grid-cols-4 place-items-center  text-medium text-almostWhite">
-        <li className="block h-full w-full border-b-2 border-purple px-2 py-4 text-center text-[14px]">
-          New
-        </li>
-        <li className="block h-full w-full  px-2 py-4 text-center text-[14px]">
-          Most Liked
-        </li>
-        <li className="block h-full w-full  px-2 py-4 text-center text-[14px]">
-          Most Comments
-        </li>
-        <li className="block h-full w-full  px-2 py-4 text-center text-[14px]">
-          Active
-        </li>
+        {filters.map((filter, i) => (
+          <li
+            className={`block h-full w-full ${filter == selectedFilter ? "border-b-2 border-purple" : ""} text-center text-[14px]`}
+            key={i}
+          >
+            <Link href={`?filter=${filter}`}>
+              <Button
+                className="h-full w-full bg-darkGray px-2  py-4"
+                radius="none"
+              >
+                {filter.charAt(0).toUpperCase() + filter.slice(1)}
+              </Button>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
