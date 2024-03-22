0a181a5d110ed9fd3ac3d3e28ef46b944c233d89
Dominik Croci <dominikrcroci@gmail.com>
2024-03-22 15:21:04 -0700

    add padding to footer (#132)

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="col-span-full flex h-[70px] items-center border-t-1 border-darkGray text-medGray">
+    <footer className="col-span-full flex h-[70px] items-center border-t-1 border-darkGray p-2 text-medGray xl:px-0">
       <p>
         Built by{" "}
         <a
