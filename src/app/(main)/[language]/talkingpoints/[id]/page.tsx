cf3d4601eedd86e42b3b6b14854fe92b80e70e53
Dominik Croci <dominikrcroci@gmail.com>
2024-04-10 00:00:53 -0700

    adjust what is shown on skeleton loading (#200)

--- src/app/(main)/[language]/talkingpoints/[id]/page.tsx
+++ src/app/(main)/[language]/talkingpoints/[id]/page.tsx
@@ -4,13 +4,36 @@ import { fetchCommentsByPostId } from "@/app/db/queries/comments";
 import CommentList from "@/components/talkingPoints/CommentList";
 import { Suspense } from "react";
 import LoadingSuspense from "@/components/common/LoadingSuspense";
+import { Skeleton, Spacer } from "@nextui-org/react";
 
 function ShowTalkingPointPage({ params }: any) {
   const { id } = params;
   return (
     <>
       <main className="col-start-1 col-end-6 px-2 lg:col-start-2 lg:col-end-5">
-        <Suspense fallback={<LoadingSuspense />}>
+        <Suspense
+          fallback={
+            <Skeleton>
+              <div className="flex min-h-[120px] flex-row items-center gap-8 rounded px-6  py-2">
+                <div className="flex h-full w-full flex-col justify-between">
+                  <div>
+                    <div className="mb-2 flex items-center gap-3">
+                      <span className="text-small text-medGray"></span>
+                    </div>
+
+                    <div>
+                      <h3 className="mb-2 flex flex-col text-xl font-semibold text-white"></h3>
+
+                      <p className="  w-full break-words bg-gradient-to-b from-medGray from-55% to-transparent bg-clip-text font-normal leading-relaxed text-transparent"></p>
+                    </div>
+                  </div>
+                  <div className="mt-2 flex items-center gap-4">Comment</div>
+                </div>
+              </div>
+              <Spacer y={10} />
+            </Skeleton>
+          }
+        >
           <ShowTalkingPoint talkingPointId={id} />
         </Suspense>
         <Suspense fallback={<LoadingSuspense />}>
--- src/components/common/LoadingSuspense.tsx
+++ src/components/common/LoadingSuspense.tsx
@@ -4,7 +4,64 @@ function LoadingSuspense() {
   return (
     <div className="flex flex-col gap-4">
       <Skeleton>
+        <div className="flex min-h-[120px] flex-row items-center gap-8 rounded bg-black  px-6 py-2">
+          <div className="flex h-full w-full flex-col justify-between">
+            <div>
+              <div className="mb-2 flex items-center gap-3">
+                <span className="text-small text-medGray"></span>
+              </div>
+
+              <div>
+                <h3 className="mb-2 flex flex-col text-xl font-semibold text-white"></h3>
+
+                <p className="  w-full break-words bg-gradient-to-b from-medGray from-55% to-transparent bg-clip-text font-normal leading-relaxed text-transparent"></p>
+              </div>
+            </div>
+            <div className="mt-2 flex items-center gap-4">Comment</div>
+          </div>
+        </div>
+        <Spacer y={10} />
+      </Skeleton>
+      <Skeleton>
         <div className="flex min-h-[120px] flex-row items-center gap-8 rounded px-6  py-2">
+          <div className="flex h-full w-full flex-col justify-between">
+            <div>
+              <div className="mb-2 flex items-center gap-3">
+                <span className="text-small text-medGray"></span>
+              </div>
+
+              <div>
+                <h3 className="mb-2 flex flex-col text-xl font-semibold text-white"></h3>
+
+                <p className="  w-full break-words bg-gradient-to-b from-medGray from-55% to-transparent bg-clip-text font-normal leading-relaxed text-transparent"></p>
+              </div>
+            </div>
+            <div className="mt-2 flex items-center gap-4">Comment</div>
+          </div>
+        </div>
+        <Spacer y={10} />
+      </Skeleton>
+      <Skeleton>
+        <div className="flex min-h-[120px] flex-row items-center gap-8 rounded px-6  py-2">
+          <div className="flex h-full w-full flex-col justify-between">
+            <div>
+              <div className="mb-2 flex items-center gap-3">
+                <span className="text-small text-medGray"></span>
+              </div>
+
+              <div>
+                <h3 className="mb-2 flex flex-col text-xl font-semibold text-white"></h3>
+
+                <p className="  w-full break-words bg-gradient-to-b from-medGray from-55% to-transparent bg-clip-text font-normal leading-relaxed text-transparent"></p>
+              </div>
+            </div>
+            <div className="mt-2 flex items-center gap-4">Comment</div>
+          </div>
+        </div>
+        <Spacer y={10} />
+      </Skeleton>
+      <Skeleton>
+        <div className="flex min-h-[120px] flex-row items-center gap-8 rounded bg-black  px-6 py-2">
           <div className="flex h-full w-full flex-col justify-between">
             <div>
               <div className="mb-2 flex items-center gap-3">
