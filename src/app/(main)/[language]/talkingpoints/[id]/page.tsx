a491c81dad70bc1368e0ab2ef55087fa51863367
Dominik Croci <dominikrcroci@gmail.com>
2024-04-11 00:13:18 -0700

    update skeleton (#183)

--- src/app/(main)/[language]/talkingpoints/[id]/page.tsx
+++ src/app/(main)/[language]/talkingpoints/[id]/page.tsx
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 import LoadingSuspense from "@/components/common/LoadingSuspense";
 import { Skeleton, Spacer } from "@nextui-org/react";
 
-function ShowTalkingPointPage({ params }: any) {
+async function ShowTalkingPointPage({ params }: any) {
   const { id } = params;
   return (
     <>
@@ -36,7 +36,29 @@ function ShowTalkingPointPage({ params }: any) {
         >
           <ShowTalkingPoint talkingPointId={id} />
         </Suspense>
-        <Suspense fallback={<LoadingSuspense />}>
+        <Suspense
+          fallback={
+            <Skeleton>
+              <div className="flex min-h-[120px] flex-row items-center gap-8 rounded px-6  py-2">
+                <div className="flex h-full w-full flex-col justify-between">
+                  <div>
+                    <div className="mb-2 flex items-center gap-3">
+                      <span className="text-small text-medGray"></span>
+                    </div>
+
+                    <div>
+                      <h3 className="mb-2 flex flex-col text-xl font-semibold text-white"></h3>
+
+                      <p className="  w-full break-words bg-gradient-to-b from-medGray from-55% to-transparent bg-clip-text font-normal leading-relaxed text-transparent"></p>
+                    </div>
+                  </div>
+                  <div className="mt-2 flex items-center gap-4">Comment</div>
+                </div>
+              </div>
+              <Spacer y={10} />
+            </Skeleton>
+          }
+        >
           <CommentCreateForm talkingPointId={params.id} startOpen />
         </Suspense>
         <Suspense fallback={<LoadingSuspense />}>
